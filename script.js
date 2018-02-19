89bb2c682c8495d25dff17960c91394e7ed44edf
breezedoc <21306570@student.uwl.ac.uk>
2018-02-19 21:35:49 +0000

    Version 0.3.1

---
+++ script.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//         JAVASCRIPT          //
+
+var hello, bye;
+var greeting = "Hey";
+var linebreak = "<br />";
+
+function contactUs()
+{
+    alert(greeting);
+}
+
+function sample()
+{
+
+    
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
